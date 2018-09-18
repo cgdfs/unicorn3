26854324260923dfc956d2ee4e207389f18e710c
unknown <krushnatkhawale@gmail.com>
2018-09-18 11:59:51 +0100

    enabled sonar plugin and added sonario credentials

--- 01-resource-nodb/src/main/java/com/example/reactive/util/ResourceUtil.java
+++ 01-resource-nodb/src/main/java/com/example/reactive/util/ResourceUtil.java
@@ -1,19 +1,9 @@
 package com.example.reactive.util;
 
 
-import com.example.reactive.model.MappedResponse;
 import com.example.reactive.model.Resource;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import jdk.nashorn.internal.ir.ObjectNode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.json.JsonParser;
-
-import java.io.IOException;
-import java.util.List;
-import java.util.Random;
 
 import static java.lang.String.format;
 import static java.lang.String.valueOf;
@@ -39,24 +29,7 @@ public class ResourceUtil {
     }
 
     private static void seeIfExceptionalCase(Long id) {
-        if(id%19==0)
+        if (id % 19 == 0)
             throw new RuntimeException("Excptional case: " + id);
-    }
-
-    public static Resource mapToResource(JsonNode jsonNode) {
-        try {
-            Long id = (new Random()).nextInt();
-            String name = jsonNode.rget("name").asText();
-            String path = "";
-            Long size = 0L;
-            return new Resource(id, name, path, size);
-        } catch (Exception e) {
-            return null;
-        }
-    }
-
-    public static MappedResponse saveResource(Resource resource) {
-        sleepForRandomTime(resource.getId());
-        return new MappedResponse(resource.getId(),"processed");
     }
 }
